9a6b3404d84cedd36e412cfe39908fd9f486a5cb
Abhijeet Kushwaha <abhijeetkushwaha1213@gmail.com>
2025-06-18 11:54:29 +0530

    Deleted ProfileGrid.tsx

--- src/data/personalityTags.ts
+++
@@ -1,9 +0,0 @@
-
-export const personalityTags = [
-  'Adventurous', 'Ambitious', 'Artistic', 'Charismatic', 'Chill', 'Confident', 
-  'Creative', 'Curious', 'Easygoing', 'Energetic', 'Funny', 'Generous', 
-  'Geeky', 'Honest', 'Humble', 'Intellectual', 'Introverted', 'Extroverted', 
-  'Kind', 'Loyal', 'Logical', 'Optimistic', 'Pessimistic', 'Passionate', 
-  'Practical', 'Quiet', 'Romantic', 'Sarcastic', 'Shy', 'Spontaneous', 
-  'Serious', 'Witty',
-];
